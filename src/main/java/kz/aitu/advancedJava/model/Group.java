6e4655f70abed76d136b374a6872ee0b79bdeb03
olki <olki92@gmail.com>
2020-09-09 10:00:22 +0600

    first commit with jpa and controller

---
+++ src/main/java/kz/aitu/advancedJava/model/Group.java
@@ -0,0 +1,11 @@
+package kz.aitu.advancedJava.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Group {
+    private long id;
+    private String name;
+}
---
+++ src/main/java/kz/aitu/advancedJava/model/Student.java
@@ -0,0 +1,25 @@
+package kz.aitu.advancedJava.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    private long id;
+    private String name;
+    private String phone;
+
+    private long groupId;
+
+}
